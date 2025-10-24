6220337a91a5422c26fc4142b1bc18316210fa9c
Alfiya-Anjum <125040235+Alfiya-Anjum@users.noreply.github.com>
2025-10-25 00:49:32 +0300

    Synapse Analytics workspace

---
+++ Synapse Analytics workspace.sql
@@ -0,0 +1,96 @@
+-- CREATE VIEW CALENDAR
+CREATE OR ALTER VIEW gold.calendar
+AS
+SELECT 
+    * 
+FROM 
+    OPENROWSET
+        (
+            BULK 'https://alstoragedatalake.blob.core.windows.net/silver/AdventureWorks_Calendar/',
+            FORMAT = 'PARQUET'
+        ) AS QUER1;
+GO
+
+
+-- CREATE VIEW CUSTOMERS
+CREATE VIEW gold.customers
+AS
+SELECT 
+    * 
+FROM 
+    OPENROWSET
+        (
+            BULK 'https://alstoragedatalake.blob.core.windows.net/silver/AdventureWorks_Customers/',
+            FORMAT = 'PARQUET'
+        ) AS QUER1;
+GO
+
+
+-- CREATE VIEW PRODUCTS
+CREATE VIEW gold.products
+AS
+SELECT 
+    * 
+FROM 
+    OPENROWSET
+        (
+            BULK 'https://alstoragedatalake.blob.core.windows.net/silver/AdventureWorks_Products/',
+            FORMAT = 'PARQUET'
+        ) AS QUER1;
+GO
+
+
+-- CREATE VIEW RETURNS
+CREATE VIEW gold.returns
+AS
+SELECT 
+    * 
+FROM 
+    OPENROWSET
+        (
+            BULK 'https://alstoragedatalake.blob.core.windows.net/silver/AdventureWorks_Returns/',
+            FORMAT = 'PARQUET'
+        ) AS QUER1;
+GO
+
+
+-- CREATE VIEW SALES
+CREATE VIEW gold.sales
+AS
+SELECT 
+    * 
+FROM 
+    OPENROWSET
+        (
+            BULK 'https://alstoragedatalake.blob.core.windows.net/silver/AdventureWorks_Sales/',
+            FORMAT = 'PARQUET'
+        ) AS QUER1;
+GO
+
+
+-- CREATE VIEW SUBCAT
+CREATE VIEW gold.subcat
+AS
+SELECT 
+    * 
+FROM 
+    OPENROWSET
+        (
+            BULK 'https://alstoragedatalake.blob.core.windows.net/silver/AdventureWorks_SUbCategories/',
+            FORMAT = 'PARQUET'
+        ) AS QUER1;
+GO
+
+
+-- CREATE VIEW TERRITORIES
+CREATE VIEW gold.territories
+AS
+SELECT 
+    * 
+FROM 
+    OPENROWSET
+        (
+            BULK 'https://alstoragedatalake.blob.core.windows.net/silver/AdventureWorks_Territories/',
+            FORMAT = 'PARQUET'
+        ) AS QUER1;
+GO
